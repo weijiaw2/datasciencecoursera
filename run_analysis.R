2ea2a2349dcda7b406b0d4ef9dbd753df950830a
weijiaw2 <74206254+weijiaw2@users.noreply.github.com>
2021-10-13 08:43:16 -0400

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,13 +1,6 @@
 # Getting and Cleaning Data Project
 # Author: Weijia Wang
 # 1. Merges the training and the test sets to create one data set.
-# 2. Extracts only the measurements on the mean and sd for each measurement.
-# 3. Users descriptive activity names to name the activities in the data set.
-# 4. Appropriately labels the data set with descriptive variable names. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
-# 5.
-# Getting and Cleaning Data Project
-# Author: Weijia Wang
-# 1. Merges the training and the test sets to create one data set.
 # 2. Extracts only the measurements on the mean and standard deviation for each measurement.
 # 3. Uses descriptive activity names to name the activities in the data set.
 # 4. Appropriately labels the data set with descriptive variable names.
